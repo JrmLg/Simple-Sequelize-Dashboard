53dc6fac1f54abc31204d3a286b64d24c4f817bc
JrmLg <63566435+JrmLg@users.noreply.github.com>
2024-01-18 11:28:20 +0100

    Initialise npm avec les dépendences du projet.

---
+++ index.js
@@ -0,0 +1,11 @@
+class SimpleDash {
+    /**
+     * @param {Sequelize} sequelize instance
+     * @param {Object} options
+     * */
+    constructor(sequelize, options) {
+        //
+    }
+}
+
+module.exports = SimpleDash
